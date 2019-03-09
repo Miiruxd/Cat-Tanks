b0829103cd15bc6f15e9774599c20907e516c14c
Xenohell <vodnarana@yahoo.com>
2019-03-09 14:44:52 +0000

    Cannon Shoots and detects collision

---
+++ Assets/Scripts/Bullet_spawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_spawn : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+
+    void FixedUpdate()
+    {
+        bool shoot = Input.GetButtonDown("Fire1");
+
+        if (shoot) Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+    }
+}
