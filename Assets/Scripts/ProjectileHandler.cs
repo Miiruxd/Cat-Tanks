3ab09ae335c17a1ef77820c8075bbf14d4cc8b58
Miiruxd <miruna.milu@gmail.com>
2019-04-01 11:45:26 +0100

    Started Volume implementation (not finished - switching comps)

--- Assets/Scripts/ProjectileHandler.cs
+++ Assets/Scripts/ProjectileHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ProjectileHandler : MonoBehaviour
 {
+    private GameObject levelManager;
     private GameObject cam;
 
     void Start()
@@ -14,5 +15,20 @@ public class ProjectileHandler : MonoBehaviour
     void Update()
     {
         cam.GetComponent<Transform>().SetPositionAndRotation(new Vector3(transform.position.x + 2.3f, cam.transform.position.y, cam.transform.position.z), cam.transform.rotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Ground")
+        {
+            Invoke("Remove", 1.0f);
+        }
+    }
+
+    void Remove()
+    {
+        gameObject.SetActive(false);
+        levelManager = GameObject.FindGameObjectWithTag("LevelManager");
+        levelManager.GetComponent<LevelManager>().TransitionCameraToTheLeft();
     }
 }
---
+++ Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeHandler : MonoBehaviour
+{
+    private GameObject levelManager;
+    private AudioSource audioSrc;
+    private float musicVolume = 0.5f;
+
+    void Start()
+    {
+        levelManager = GameObject.FindGameObjectWithTag("LevelManager");
+        audioSrc = levelManager.GetComponent<AudioSource>();
+        audioSrc.volume = musicVolume;
+    }
+
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
